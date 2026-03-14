763a3242e57c13d3e7c342a79b921b7a60324847
Surya <166209262+suryasekar02@users.noreply.github.com>
2026-03-14 19:47:35 +0530

    Update BackendAPI.js

--- BackendAPI.js
+++ BackendAPI.js
@@ -16,12 +16,11 @@ app.use((req, res, next) => {
 });
 
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
-  });
+  host: "sql201.infinityfree.com",
+  user: "if0_41390102",
+  password: "nuAAHUs7bx",
+  database: "if0_41390102_jewellery"
+});
 
 db.connect((err) => {
     if (err) {
